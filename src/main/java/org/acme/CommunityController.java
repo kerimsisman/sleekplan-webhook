aa3f40c51fa5984f4492be84e7423f06b07263a2
kerim.sisman <kerim_sisman@hotmail.com>
2023-12-19 00:20:11 +0300

    logger added
    Signed-off-by: kerim.sisman <kerim_sisman@hotmail.com>

--- src/main/java/org/acme/CommunityController.java
+++ src/main/java/org/acme/CommunityController.java
@@ -3,11 +3,13 @@ package org.acme;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.QueryParam;
+import lombok.extern.slf4j.Slf4j;
 
 @Path("/api/v1/community/webhooks")
+@Slf4j
 public class CommunityController {
 	@POST
 	public void webhooks(@QueryParam(value = "key") String key, String payload) {
-		System.out.println(String.format("key:[%s] payload:[%s]", key, payload));
+		log.info(String.format("key:[%s] payload:[%s]", key, payload));
 	}
 }
