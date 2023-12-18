d15842b64b340b3ee9e2c329015f098929548268
kerim.sisman <kerim_sisman@hotmail.com>
2023-12-18 16:14:29 +0300

    log eklendi
    Signed-off-by: kerim.sisman <kerim_sisman@hotmail.com>

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasyXX";
+    	System.out.println("Deleteee");
+        return "Hello RESTEasy";
     }
 }
